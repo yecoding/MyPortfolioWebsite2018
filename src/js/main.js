226c48b351ff571012d594d5b8b80fc8c43f6439
Ye Wang <wangye.tju@gmail.com>
2018-04-12 19:07:48 -0400

    Improved Animations and responsive design

--- src/js/main.js
+++ src/js/main.js
@@ -1,21 +1,59 @@
-$("#togglediv").click(function(){
-	$("#togglediv").toggleClass("active");
-	$("#mainnav").slideToggle();
+//Mobile nav button
+
+var toggleBtn = document.getElementById("togglediv");
+var mainnav = document.getElementById('mainnav');
+toggleBtn.addEventListener("click", function(){
+	toggleBtn.classList.toggle("active");
+	console.log('javascript');
+	if(mainnav.style.bottom ==='' || mainnav.style.bottom ==='100%'){
+		mainnav.style.bottom="0";
+	}else{
+		mainnav.style.bottom = "100%";
+	}
 });
 
-imageGallery("#linkStore", "#onlineStore");
-imageGallery("#linkCity", "#cityFactApp");
-imageGallery("#linkJS", "#jsPractice");
-imageGallery("#linkTodo", "#todoAppMobile");
-imageGallery("#linkInvestecs", "#investecsWebsite");
-imageGallery("#link8188", "#yonge8188Website");
-imageGallery("#linkPortfolio", "#portfolioWebsite");
+//Portfolio Gallery
 
-function imageGallery(thumbnailID, itemID){
-	$(thumbnailID).click(function(){
-		$(".work-item").hide();
-		$(itemID).show();
-		$(".thumbnail-item").removeClass("active-item");
-		$(thumbnailID).addClass("active-item");
-	})
-}
+var workItems = [
+	{thumbnailID: "linkStore",
+	itemID:"onlineStore"
+	},
+	{thumbnailID: "linkCity",
+	itemID:"cityFactApp"
+	},
+	{thumbnailID: "linkJS",
+	itemID:"jsPractice"
+	},
+	{thumbnailID: "linkTodo",
+	itemID:"todoAppMobile"
+	},
+	{thumbnailID: "linkInvestecs",
+	itemID:"investecsWebsite"
+	},
+	{thumbnailID: "link8188",
+	itemID:"yonge8188Website"
+	},
+	{thumbnailID: "linkPortfolio",
+	itemID:"portfolioWebsite"
+	}
+];
+
+var allWorks = document.getElementsByClassName("work-item");
+var allThumbnails = document.getElementsByClassName("thumbnail-item");
+
+workItems.forEach(function(item){
+		var thumbnail = document.getElementById(item.thumbnailID);
+		var selectWork = document.getElementById(item.itemID);
+
+		thumbnail.addEventListener("click", function(){
+			for(i=0;i<allWorks.length;i++){
+				allWorks[i].style.height = "0";
+			}
+			selectWork.style.height = "100%";
+			for(i=0;i<allThumbnails.length;i++){
+				allThumbnails[i].classList.remove("active-item");
+			}
+			thumbnail.classList.add("active-item");		
+		})
+	}
+);
